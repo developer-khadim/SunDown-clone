0d88bdcf83ab2269791425707a15aba426b4e47b
developer-khadim <khadimalisoomro33.com>
2024-06-09 23:47:18 +0800

    page 2 add

--- main.js
+++ main.js
@@ -1,4 +1,30 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
-});
+
+var scroll = new LocomotiveScroll({
+            el: document.querySelector('[data-scroll-container]'),
+            smooth: true
+        });
+        gsap.registerPlugin(ScrollTrigger);
+        gsap.to(".con", {
+            scrollTrigger: {
+                trigger: ".con",
+                start: "top 80%",
+                end: "bottom 80%",
+                scrub: 1,
+                pin: true,
+                pinSpacing: false
+            },
+            y: 0,
+            duration: 1
+        });
+        gsap.to(".con", {
+            scrollTrigger: {
+                trigger: ".con",
+                start: "top 80%",
+                end: "bottom 80%",
+                scrub: 1,
+                pin: true,
+                pinSpacing: false
+         } ,
+            y: 0,
+            duration: 1
+        });
